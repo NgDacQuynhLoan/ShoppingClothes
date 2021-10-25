3d540aaa3497d3ac5a04d81932539cc0da70dc03
Loan Nguyen <loannguyen1456@gmail.com>
2021-10-26 01:20:04 +0700

    update shopping cart

--- js/shopping-cart.js
+++ js/shopping-cart.js
@@ -1,60 +1,3 @@
-// function main() {
-//     var slideIndex = 1;
-//     showSlides(slideIndex);
-// }
-
-// main();
-
-/* remove */
-function removeCartItem(removingItem) {
-    let currentItem = removingItem.parentNode.parentNode;
-    if (confirm('Do you want to remove this product?')){
-        currentItem.remove();
-    }else{
-        console.log('not remove')
-    }
-    // updatedTotal ();
-}
-function sumArray(mang){
-        let sum = 0;
-        for (let i = 0; i < mang.length; i++){
-            sum += mang[i];
-        }
-         
-        return sum;
-}
-
-
-/* Not finish yet */
-// function bill(totalItems){
-//     let sum = 0
-//     // let totalCost = totalItems.parentNode.parentNode.parentNode.parentNode.firstElementChild.
-// nextElementSibling.firstElementChild.lastChild.firstElementChild;
-    
-//     for(i=0 ; i< totalItems.length ; i++){
-//         total = totalItems[i]    
-//     }
-// }
-/* total ===========================> Sai */ 
-function updateTotal(itemInput){
-    let currentItemPrice = itemInput.parentNode.previousElementSibling.firstElementChild;
-    let currentItemTotal = itemInput.parentNode.nextElementSibling.firstElementChild;
-    let formattedItemQuantity = Number(itemInput.value);
-    let formattedItemPrice = Number(currentItemPrice.innerHTML.replace('$',''));
-
-    // Calculate current item total
-    currentItemTotal = formattedItemQuantity * formattedItemPrice;
-
-    // Re-format current item total
-    let reverseFormattedItemTotal = "".concat('$', "".concat(currentItemTotal, ".00"));
-
-    // Update current item total
-    itemInput.parentNode.nextElementSibling.firstElementChild.innerHTML = reverseFormattedItemTotal;
-    
-}
-
-
-
 /* Nav */
 function openNav() {
     document.getElementById("myNav").style.width = "150px";
@@ -70,12 +13,6 @@ function popUp(){
     document.getElementById("log-in").classList.toggle("active");
 }
 
-/* 
-    - elem.classList.add/remove(“class”) – thêm / bớt lớp.
-    - elem.classList.toggle(“class”) – thêm lớp nếu nó không tồn tại, nếu không 
-    thì xóa nó.
-    - elem.classList.contains(“class”)- kiểm tra lớp đã cho, trả về true/false. 
-*/
 
 function correctLogin (){
     var account = document.getElementById('email').value
@@ -92,4 +29,69 @@ function correctLogin (){
 }
 
 
+
+
+/* remove */
+
+function removeCartItem(removingItem) {
+    let currentItem = removingItem.parentNode.parentNode;
+    if (confirm('Do you want to remove this product?')){
+        currentItem.remove();
+    }else {
+        console.log('not remove')
+    }
+}//Remove button
+function removeEmptyCart(removeEmpty){
+    let removeItem = Number(removeEmpty.value);
+    let qty = removeEmpty.parentNode.parentNode;
+    if (removeItem === 0 && confirm('Do you want to remove this product?')){
+        
+        qty.remove();
+    }else {
+        console.log('not remove')
+    }
+}//Remove empty qty
+
+function totalCost (){
+    var priceOfAllProduct = document.querySelectorAll('.tag-total .tag-total-price')
+    var sum = 0;
+    for (let itemPrice of Object.values(priceOfAllProduct)) {
+        temp1 = Number(itemPrice.innerHTML.replace('$',''));
+        sum += temp1
+        
+    }
+    let  totalCost = document.querySelector('#all-total')
+    
+    
+    totalCost.replaceWith(`$${sum}.00`) 
+
+    
+}   
+
+
+function updateTotal(itemInput){
+    let currentItemPrice = itemInput.parentNode.previousElementSibling.firstElementChild;
+    let currentItemTotal = itemInput.parentNode.nextElementSibling.firstElementChild;
+    let formattedItemQuantity = Number(itemInput.value);
+    let formattedItemPrice = Number(currentItemPrice.innerHTML.replace('$',''));
+    // Calculate current item total
+    currentItemTotal = formattedItemQuantity * formattedItemPrice;
+
+    // Re-format current item total
+    let reverseFormattedItemTotal = "".concat('$', "".concat(currentItemTotal, ".00"));
+
+    // Update current item total
+    itemInput.parentNode.nextElementSibling.firstElementChild.innerHTML = reverseFormattedItemTotal;
+}
+
+
+
+
+
+/* 
+    - elem.classList.add/remove(“class”) – thêm / bớt lớp.
+    - elem.classList.toggle(“class”) – thêm lớp nếu nó không tồn tại, nếu không 
+    thì xóa nó.
+    - elem.classList.contains(“class”)- kiểm tra lớp đã cho, trả về true/false. 
+*/
 
